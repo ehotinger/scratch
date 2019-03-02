68c4a55a587dfa9b6d7f0635b43d7fe7d2fc883d
Eric Hotinger <ehotinger@gmail.com>
2019-03-02 09:52:10 -0800

    Update to Go 1.12

--- main.go
+++ main.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
 )
 
 func main() {
-	var vals = flag.String("values", "", "Values JSON data")
-	var run = flag.String("run", "", "Values JSON data")
-	flag.Parse()
-
-	log.Println("Vals:", *vals)
-	log.Println("Run:", *run)
-
-	var valsObjMap map[string]*json.RawMessage
-	if err := json.Unmarshal([]byte(*vals), &valsObjMap); err != nil {
-		log.Fatalf("vals unmarshalling err: %v", err)
-	}
-
-	var runObjMap map[string]*json.RawMessage
-	if err := json.Unmarshal([]byte(*run), &runObjMap); err != nil {
-		log.Fatalf("run unmarshalling err: %v", err)
-	}
-
-	fmt.Println("Deserializing Values...")
-	valsOut, err := json.Marshal(valsObjMap)
-	fmt.Println(string(valsOut), "Err:", err)
-
-	fmt.Println("Deserializing Run...")
-	runOut, err := json.Marshal(runObjMap)
-	fmt.Println(string(runOut), "Err:", err)
+	fmt.Println("Hello world!")
 }
--- main_old.go
+++
@@ -1,5 +0,0 @@
-package main
-
-// func main() {
-// 	fmt.Println("This is an empty scratch project")
-// }
